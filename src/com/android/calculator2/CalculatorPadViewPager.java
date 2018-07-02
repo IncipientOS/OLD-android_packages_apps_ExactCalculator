LineageOS/android_packages_apps_ExactCalculator

Themes: Expose hard coded background in java
-This backgound is not seen and is blocking clear themes from showing the wallpaper

Change-Id: I6248f1edfa97b02c34604059f55454248dc1cc98
Signed-off-by: Bryan Owens <djbryan3540@gmail.com>

src/com/android/calculator2/CalculatorPadViewPager.java
@@ -17,6 +17,7 @@
 package com.android.calculator2;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -89,10 +90,11 @@ public class CalculatorPadViewPager extends ViewPager {
     }
 
     public CalculatorPadViewPager(Context context, AttributeSet attrs) {
+        Resources res = getContext().getResources();
         super(context, attrs);
 
         setAdapter(mStaticPagerAdapter);
-        setBackgroundColor(Color.BLACK);
+        setBackgroundColor(res.getColor(R.color.pad_view_pager_background));
         setPageMargin(getResources().getDimensionPixelSize(R.dimen.pad_page_margin));
         setPageTransformer(false, mPageTransformer);
         addOnPageChangeListener(mOnPageChangeListener);

IncipientOS/OLD-android_packages_apps_ExactCalculator

Change-Id: I30bade7c82f29767c6659c8a75bb402d245e49f1

src/com/android/calculator2/CalculatorPadViewPager.java
@@ -92,7 +92,7 @@ public class CalculatorPadViewPager extends ViewPager {
         super(context, attrs);
 
         setAdapter(mStaticPagerAdapter);
-        setBackgroundColor(Color.BLACK);
+        setBackgroundColor(context.getColor(R.color.pad_view_pager_background));
         setPageMargin(getResources().getDimensionPixelSize(R.dimen.pad_page_margin));
         setPageTransformer(false, mPageTransformer);
         addOnPageChangeListener(mOnPageChangeListener);